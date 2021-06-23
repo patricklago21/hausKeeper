e391bfc22de13117c13072ce5d7b7395d78d57c8
Arnaut <lou.arnaut@gmail.com>
2021-06-23 11:46:07 -0700

    added hauskeepr route to api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./client-routes.js');
+const hauskeeprRoutes = require('./hauskeepr-routes.js');
 
 router.use('/clients', userRoutes);
+router.use('/hauskeeprs', hauskeeprRoutes);
 
 module.exports = router;
