95ee13d06d94537b667b41aaf7eed58a743335ce
Arnaut <lou.arnaut@gmail.com>
2021-06-23 11:49:57 -0700

    added profession routes to api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./client-routes.js');
 const hauskeeprRoutes = require('./hauskeepr-routes.js');
+const professionRoutes = require('./prefession-routes.js');
 
 router.use('/clients', userRoutes);
 router.use('/hauskeeprs', hauskeeprRoutes);
+router.use('/professions', professionRoutes);
 
 module.exports = router;
