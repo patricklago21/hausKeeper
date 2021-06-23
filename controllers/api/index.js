a872a57003b8409674b33130f50c0bd8a301c362
Arnaut <lou.arnaut@gmail.com>
2021-06-23 12:29:33 -0700

    added reviews route to api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const userRoutes = require('./client-routes');
 const hauskeeprRoutes = require('./hauskeepr-routes');
 const professionRoutes = require('./profession-routes');
 const appointmentRoutes = require('./appointment-routes');
+const reviewRoutes = require('./review-routes');
 
 router.use('/clients', userRoutes);
 router.use('/hauskeeprs', hauskeeprRoutes);
 router.use('/professions', professionRoutes);
 router.use('/appointments', appointmentRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
