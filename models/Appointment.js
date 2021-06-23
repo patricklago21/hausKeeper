87a5c062d114a97c02ac70657e40bdf7d73ab422
Cerebreo <nellirism@outlook.com>
2021-06-22 20:15:24 -0700

    Making sure that appointment is green

--- models/Appointment.js
+++ models/Appointment.js
@@ -2,6 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class Appointment extends Model {
+}
+
 Appointment.init(
     {
         appointment_id: {
