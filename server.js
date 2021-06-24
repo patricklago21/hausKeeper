39b010adcd4da0ce2422afe49f1fe0c095545a92
Cerebreo <nellirism@outlook.com>
2021-06-24 10:30:17 -0700

    turned off sequelize sync at line 39 of server.js

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 const controllers = require('./controllers/');
 app.use(controllers);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
