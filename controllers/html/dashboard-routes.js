d74ac2404bae479970b8a422801c1f159a999df0
Cerebreo <nellirism@outlook.com>
2021-06-25 20:35:06 -0700

    added the dashboard functionality with all the needed fields

--- controllers/html/dashboard-routes.js
+++ controllers/html/dashboard-routes.js
@@ -1,9 +1,42 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
+const { Hauskeepr, Profession } = require('../../models');
 
 // render dashboard page
 router.get('/', (req, res) => {
-  res.render('dashboard', {});
-});
+    Hauskeepr.findAll({
+            attributes: {
 
+                exclude: ['password'],
+                include: [
+                    [
+                        sequelize.literal('(SELECT AVG(stars) FROM review WHERE review.hauskeepr_id = hauskeepr.id)'),
+                        'rating'
+                    ]
+
+                ]
+
+            },
+            include: [{
+                model: Profession,
+                attributes: [
+                    'profession_name'
+                ]
+            }]
+
+        })
+        .then(dbData => {
+            const hauskeeprs = dbData.map(hauskeepr => hauskeepr.get({ plain: true }));
+            console.log(dbData);
+            res.render('dashboard', {
+                hauskeeprs,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,20 @@
+const sequelize = require('../config/connection');
 const seedClients = require('./client-seeds');
 const seedHauskeeprs = require('./hauskeepr-seeds');
 const seedAppointments = require('./appointment-seeds');
 const seedProfessions = require('./profession-seeds');
 const seedReviews = require('./review-seeds');
 
-const sequelize = require('../config/connection');
+const seedAll = async() => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+    await seedProfessions();
+    await seedClients();
+    await seedHauskeeprs();
+    await seedAppointments();
+    await seedReviews();
 
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('--------------');
-  await seedClients();
-  await seedHauskeeprs();
-  await seedAppointments();
-  await seedProfessions();
-  await seedReviews();
-
-  process.exit(0);
+    process.exit(0);
 };
 
 seedAll();
