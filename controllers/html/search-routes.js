739dccf0127795d9a4b43ec930f55f451db33738
Arnaut <lou.arnaut@gmail.com>
2021-06-28 14:53:35 -0700

    added advanced search

--- controllers/html/search-routes.js
+++ controllers/html/search-routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { Hauskeepr, Profession, Review, Appointment } = require('../../models');
 const withAuth = require('../../utils/auth');
 const { Op } = require('sequelize');
+
 // /search?q=search%20query
 router.get('/:q', withAuth, (req, res) => {
     Hauskeepr.findAll({
@@ -16,6 +17,45 @@ router.get('/:q', withAuth, (req, res) => {
                     // {profession_name: { [Op.like]: '%' + req.params.q + '%' }}
                 ]
             },
+            attributes: {
+                exclude: ['password'],
+                include: [
+                    [
+                        sequelize.literal('(SELECT AVG(stars) FROM review WHERE review.hauskeepr_id = hauskeepr.id)'),
+                        'rating'
+                    ]
+                ]
+            },
+            include: [{
+                    model: Review,
+                    attributes: ['id', 'review', 'stars', 'createdAt', 'updatedAt']
+                },
+                {
+                    model: Appointment,
+                    attributes: ['id', 'datetime', 'client_id', 'hauskeepr_id', 'notes', 'status', 'hours', 'total_cost']
+                },
+                {
+                    model: Profession,
+                    attributes: ['profession_name']
+                }
+            ]
+        })
+        .then(dbData => {
+            const hauskeeprs = dbData.map(hauskeepr => hauskeepr.get({ plain: true }));
+            res.render('dashboard', {
+                hauskeeprs,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// /search - all results
+router.get('/', withAuth, (req, res) => {
+    Hauskeepr.findAll({
             attributes: {
                 exclude: ['password'],
                 include: [
